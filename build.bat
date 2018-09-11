d933b93561fa81e201ff55e583e3f4cb7af80b67
Liam <parker.liam5@gmail.com>
2018-09-11 21:08:50 +1000

    compiling with vscode

---
+++ build.bat
@@ -0,0 +1,9 @@
+@echo off
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat"  x64   
+
+set compilerflags=/Od /Zi /EHsc
+set linkerflags= /link /OUT:emu.exe
+set programFiles= src/main.c src/memory.c src/z80.c
+
+cl.exe %compilerflags% %programFiles% %linkerflags%
