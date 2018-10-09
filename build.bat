4e364a794f077092b2c1102d6d58d58976f6c1e3
Liam <parker.liam5@gmail.com>
2018-10-09 22:31:22 +1100

    push

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat"  x64   
 
-set compilerflags=/Od /Zi /EHsc
+set compilerflags= /Od /Zi /EHsc
 set linkerflags= /link /out:emu.exe
 set programFiles= src/main.c src/memory.c src/z80.c
 
