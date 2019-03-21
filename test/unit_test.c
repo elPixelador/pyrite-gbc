4d106014f16b61165456f33a4d4f8f63640fb74e
Liam <parker.liam5@gmail.com>
2019-03-21 20:50:07 +1100

    Unit testing

--- test/unit_test.c
+++ test/unit_test.c
@@ -1,4 +1,32 @@
-#define mu_assert(message, test) do { if (!(test)) return message; } while (0)
-#define mu_run_test(test) do { char *message = test(); tests_run++; \
-                                if (message) return message; } while (0)
+#include "minunit.h"
+#include "cpu.h"
+#include "memory.h"
 
+static CPU* cpu;
+static Memory* memory;
+
+void test_setup(void) {
+	cpu = createCPU();
+	memory = createMemory();
+}
+
+void test_teardown(void) {
+	unloadCPU(&cpu);
+	unloadMemory(&memory);
+}
+
+MU_TEST(test_check) {
+	mu_check(5 == 7);
+}
+
+MU_TEST_SUITE(instruction_test_suite) {
+    MU_SUITE_CONFIGURE(&test_setup, &test_teardown);
+	MU_RUN_TEST(test_check);
+}
+
+int main()
+{ 
+    MU_RUN_SUITE(test_suite);
+	MU_REPORT();
+    return 0;
+} 
