dd5b2eaa03c2f1a4440e2f10b5b0bd846aea33a6
Liam Parker <parker.liam5@gmail.com>
2025-05-02 22:38:53 +1000

    Format

--- pyrite-core/src/cartridge.h
+++ pyrite-core/src/cartridge.h
@@ -1,9 +1,11 @@
 #pragma once
 #include <string>
 
-class Cartridge {
+class Cartridge
+{
 	uint8_t data[0x8000];
+
 public:
 	Cartridge(std::string path);
-	void loadROM0(uint8_t* memory);
+	void loadROM0(uint8_t *memory);
 };
